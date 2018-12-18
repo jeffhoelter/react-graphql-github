e7c4b06d576ef28c7a7b2f5fb6ff7257a8bf67ed
Jeff Hoelter <jeffh@loar.net>
2018-12-18 10:26:53 -0500

    Added simple GraphQL query to GitHub's API via axios. Removed package-lock in favor of yarn.lock.

--- src/App.js
+++ src/App.js
@@ -12,14 +12,94 @@ const axiosGitHubGraphQL = axios.create({
 
 const Title = "React GraphQL GitHub Client";
 
+const GET_ORGANIZATION = `
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+      repository(name: "the-road-to-learn-react")
+    }
+  }
+`;
+
 class App extends Component {
+  state = {
+    path: "the-road-to-learn-react/the-road-to-learn-react",
+    organization: null,
+    errors: null
+  };
+
+  componentDidMount = () => {
+    // fetch data
+    this.onFetchFromGitHub();
+  };
+
+  onChange = event => {
+    this.setState({ path: event.target.value });
+  };
+
+  onSubmit = event => {
+    // fetch data
+
+    event.preventDefault();
+  };
+
+  onFetchFromGitHub = () => {
+    axiosGitHubGraphQL.post("", { query: GET_ORGANIZATION }).then(result =>
+      this.setState({
+        organization: result.data.data.organization,
+        errors: result.data.errors
+      })
+    );
+  };
+
   render() {
+    const { path, organization, errors } = this.state;
+
     return (
       <div>
         <h1>{Title}</h1>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">Show open issues for https://github.com</label>
+          <input
+            id="url"
+            type="text"
+            value={this.path}
+            onChange={this.onSubmit}
+            style={{ width: "300px" }}
+          />
+          <button type="submit">Search</button>
+        </form>
+
+        <hr />
+        {organization ? (
+          <Organization organization={organization} errors={errors} />
+        ) : (
+          <p>No information yet.</p>
+        )}
       </div>
     );
   }
 }
+
+const Organization = ({ organization, errors }) => {
+  if (errors) {
+    return (
+      <p>
+        <strong>Something went wrong:</strong>
+        {errors.map(error => error.message).join(" ")}
+      </p>
+    );
+  }
+
+  return (
+    <div>
+      <p>
+        <strong>Issues from Organization: </strong>
+        <a href={organization.url}>{organization.name}</a>
+      </p>
+    </div>
+  );
+};
 
 export default App;
